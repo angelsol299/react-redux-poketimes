7381b0b6307b20e6b0364b95c2cbcb7da0b37e54
angelsol299 <32056957+angelsol299@users.noreply.github.com>
2018-11-01 22:41:55 +0100

    sdsd
    sff

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus
+        maxime dolores amet impedit consectetur nostrum pariatur eligendi rem
+        maiores animi excepturi quam sapiente omnis ullam, fugiat perferendis
+        quod sunt.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus
+        maxime dolores amet impedit consectetur nostrum pariatur eligendi rem
+        maiores animi excepturi quam sapiente omnis ullam, fugiat perferendis
+        quod sunt.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
