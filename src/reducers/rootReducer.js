8e0c7225cef68f797eb8605f59955c25c58d378f
angelsol299 <32056957+angelsol299@users.noreply.github.com>
2018-11-05 19:03:36 +0100

    dummy data on rootReducer
    dummy data

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,24 @@
 const initState = {
-  posts: []
+  posts: [
+    {
+      id: "1",
+      title: "vamos a la playa",
+      body:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium facilis a labore perferendis modi pariatur totam, quia velit sint rerum dolores adipisci vitae maxime vel tempore! Laudantium dicta maxime magnam?"
+    },
+    {
+      id: "2",
+      title: "me gusta escribir codigo",
+      body:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium facilis a labore perferendis modi pariatur totam, quia velit sint rerum dolores adipisci vitae maxime vel tempore! Laudantium dicta maxime magnam?"
+    },
+    {
+      id: "3",
+      title: "codigo abierto es lo mejor",
+      body:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium facilis a labore perferendis modi pariatur totam, quia velit sint rerum dolores adipisci vitae maxime vel tempore! Laudantium dicta maxime magnam?"
+    }
+  ]
 };
 
 const rootReducer = (state = initState, action) => {
